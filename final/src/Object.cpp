4058c45f5712156478d5761d4682edd467efbc27
Treek <823334594@qq.com>
2019-06-12 00:15:43 +0800

    Rename object.cpp to Object.cpp

--- final/src/Object.cpp
+++ final/src/Object.cpp
@@ -1,20 +1,20 @@
 #include "Object.h"
 
 Object::Object(vector<float> _vertices, Shader *_shader) : vertices(_vertices), shader(_shader) {
-	// Éú³É
+	// ç”Ÿæˆ
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	// °ó¶¨ VAO, VBO
+	// ç»‘å®š VAO, VBO
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STREAM_DRAW);
-	// Î»ÖÃÊôĞÔ
+	// ä½ç½®å±æ€§
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-	// ÑÕÉ«ÊôĞÔ
+	// é¢œè‰²å±æ€§
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	// ÎÆÀíÊôĞÔ
+	// çº¹ç†å±æ€§
 	glEnableVertexAttribArray(2);
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -22,21 +22,21 @@ Object::Object(vector<float> _vertices, Shader *_shader) : vertices(_vertices), 
 }
 
 Object::~Object() {
-	// ½â³ı°ó¶¨
+	// è§£é™¤ç»‘å®š
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
 }
 
-// µ¥¸öcubeäÖÈ¾
+// å•ä¸ªcubeæ¸²æŸ“
 void Object::Render(int vertices_num) {
 	glBindVertexArray(VAO);
 	glDrawArrays(GL_TRIANGLES, 0, vertices_num);
 	glBindVertexArray(0);
 }
 
-// ¶à¸öcubeäÖÈ¾
+// å¤šä¸ªcubeæ¸²æŸ“
 void Object::Render(vector<glm::vec3> positions) {
-	// äÖÈ¾¶à¸öÁ¢·½Ìå
+	// æ¸²æŸ“å¤šä¸ªç«‹æ–¹ä½“
 	glBindVertexArray(VAO);
 	for (int i = 0; i < positions.size(); i++) {
 		glm::mat4 model = glm::mat4(1.0f);
